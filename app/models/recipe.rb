6f208ac1cc798446d6ee3b4000415947733f59cf
Veronique Tureau <veronique.tureau@gmail.com>
2023-11-21 21:28:16 -0500

    Association and validations corrected

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   has_many :plannings, dependent: :destroy
   has_many :recipe_categories, dependent: :destroy
+  has_many :categories, through: :recipe_categories
 
   validates :name, :description, :preparation, presence: true
   has_one_attached :photo
