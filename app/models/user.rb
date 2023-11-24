06cb80f0200506e502641535bfcbc4e3a0b6facc
Francoise1999 <lizmentor15@gmail.com>
2023-11-23 20:32:17 -0500

    Modification dans le controlleur de planning

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :plannings, dependent: :destroy
   has_many :recipes, dependent: :destroy
+  has_many :owner_plannings, through: :recipes, source: :plannings
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
