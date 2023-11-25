7d9843b9c7ef7a5f9ab7e52699e13ce210b24e5e
Veronique Tureau <veronique.tureau@gmail.com>
2023-11-25 12:26:37 -0500

    ingredient and categories add to the new recipe form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :recipes  do
+  resources :recipes do
     resources :plannings
   end
   # Defines the root path route ("/")
