4c5e4a77370a04f08355d3f983dbcb5b75b6a633
Michael Lopes <gh api loopes.09@gmail.com>
2023-11-27 14:23:37 -0500

    Creation d'une route, controller et vue pour USERS afin de creer la page de profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   has_many :plannings, dependent: :destroy
   has_many :recipes, dependent: :destroy
   has_many :owner_plannings, through: :recipes, source: :plannings
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/UsersController'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :recipes do
     resources :plannings
   end
-
+  resources :users, only: [:show, :edit, :update]
   resources :plannings
 
   # Defines the root path route ("/")
