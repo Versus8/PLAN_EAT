0a93bf24dec195afe46d981bc7694ea2243d2de1
Veronique Tureau <veronique.tureau@gmail.com>
2023-11-28 19:23:17 -0500

    routes for first list and shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'owner_plannings', to: 'plannings#owner_planning', as: :owner_plannings
-  get "recipes_index/:category", to: "recipes#recipes_index", as: :recipies_index
+  get "recipes_index/:category", to: "recipes#recipes_index", as: :recipes_index
+  get "recipes/:id/ingredients_list", to: "recipes#recipe_ingredients_index", as: :recipe_ingredients_index
+  get "ingredients_list", to: "recipes#ingredients_list", as: :ingredients_list
 end
